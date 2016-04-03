92f8d4f6083f16eeeb27fed1f12d14c8458c1a14
Frank Pepermans <frank@igindo.com>
2016-04-03 15:00:45 +0200

    no message

--- lib/stylus_transformer.dart
+++ lib/stylus_transformer.dart
@@ -11,12 +11,6 @@ class StylusTransformer extends Transformer {
 
   String get allowedExtensions => ".html";
 
-  classifyPrimary(AssetId id) {
-    if (!id.path.endsWith('index.html')) return null;
-
-    return p.url.dirname(id.path);
-  }
-
   @override
   Future<dynamic> apply(Transform transform) {
     final Completer<int> completer = new Completer<int>();
@@ -30,7 +24,7 @@ class StylusTransformer extends Transformer {
         .transform(UTF8.decoder as StreamTransformer<List<int>, dynamic>)
         .listen((String css) {
           print(css);
-          
+
           transform.addOutput(new Asset.fromString(new AssetId('taurus_main', 'web/styles/compc_stylus.css'), css));
         });
 
